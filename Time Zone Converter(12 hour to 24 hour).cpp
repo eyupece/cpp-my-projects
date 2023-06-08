520527a2f2e69a376e2629172693efcb73112322
Eyüp Ece <110734161+eyupece@users.noreply.github.com>
2023-06-08 17:14:46 +0300

    Update Time Zone Converter(12 hour to 24 hour).cpp

--- Time Zone Converter(12 hour to 24 hour).cpp
+++ Time Zone Converter(12 hour to 24 hour).cpp
@@ -6,48 +6,41 @@ using namespace std;
 // 04:27:46PM ifadesi gibi düşünürsek 10+1 den 11 karakter olmalı.  
 
 int main() { 
+    cout << "Please enter a time in hh:mm:ssAM/PM format! "; 
+    char s[11]; 
+    cin >> s; 
 
-  cout << "Please enter a time in hh:mm:ssAM/PM format! "; 
+  if (s[8] == 'P') { 
 
-char s[11]; 
+    char x[3]; 
+    x[0] = s[0]; 
+    x[1] = s[1]; 
+    x[2] = '\0'; 
 
-  cin >> s; 
-
- if (s[8] == 'P') { 
-
-char x[3]; 
-x[0] = s[0]; 
-x[1] = s[1]; 
-
-x[2] = '\0'; 
-
-cout << (int)x[0] << endl; 
+/*  cout << (int)x[0] << endl; 
 cout << (int)x[1] << endl; 
+ASCII tablosuna göre rakamlar 48-57 */  // buradan comment işaretlerini silip değerleri kontrol et 
 
-// ASCII tablosuna göre rakamlar 48-57  
+    int saat = 0; 
+    saat += (x[0] - 48) * 10; // onlar basamağından dolayı  
+    saat += x[1] - 48; 
+    saat += 12; // + 12 sebebi 12'lik sistemden 24'lük sisteme geçiş. 
+
+    s[0] = (saat / 10) +48; // + 48 in sebebi s char türünde bir değişken ve 48-57 arasında rakamlar var. 
+    s[1] = saat%10 +48 ;  
+  
+    s[8] = '\0';
+    cout << s << endl;
+  }
+  
+  else if (s[8] == 'A') { 
+    s[8] = '\0'; 
+    cout << s << endl; 
+  } 
+  
+  else { 
+    cout << "Please available value" << endl; 
+  } 
  
-int saat = 0; 
-  
-saat += (x[0] - 48) * 10; // onlar basamağından dolayı  
-saat += x[1] - 48; 
-saat += 12; // + 12 sebebi 12'lik sistemden 24'lük sisteme geçiş. 
-
-s[0] = (saat / 10) +48; // kontrol time 
-s[1] = saat%10 +48 ;  
-
-s[8] = '\0'; 
-
-  cout << s << endl; 
- }
-  
- else if (s[8] == 'A') { 
-  s[8] = '\0'; 
-  cout << s << endl; 
- } 
-
- else { 
-  cout << "Please available value" << endl; 
- } 
-
   return 0; 
 } 
